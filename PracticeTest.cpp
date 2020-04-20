e440fb2af1a118dfe3d5dd01c41e529aabe487f4
Eric <ekiesow@mail.csuchico.edu>
2020-04-20 14:10:04 -0700

    Added some palindrome tests

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -20,3 +20,31 @@ TEST(PracticeTest, is_simple_palindrome)
     bool actual = obj.isPalindrome("aa");
     ASSERT_TRUE(actual);
 }
+
+TEST(PracticeTest, is_simplest_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("a");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_upper_lower_case_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("abBA");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, palindrome_with_spaces)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("never odd or even");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, not_a_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("racedcar");
+    ASSERT_FALSE(actual);
+}
